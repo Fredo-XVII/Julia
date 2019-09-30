eb46a6c6a8b90b9df03caa003c562d664942ccfc
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2019-09-29 21:31:17 -0500

    testing juliapro

---
+++ Install Packages.jl
@@ -0,0 +1,49 @@
+
+using Pkg
+
+Pkg.add("Plots")
+
+Pkg.add("QuantEcon")
+
+Pkg.add("JuliaDB")
+
+Pkg.add("Lazy")
+
+Pkg.add("DataFramesMeta")
+
+Pkg.add("Query")
+
+using Pkg
+Pkg.add("Plotly")
+
+using Pkg
+Pkg.add("Plots")
+
+using Pkg
+Pkg.add("RCall")
+
+Pkg.add("ScikitLearn")
+
+Pkg.add("StatsPlots")
+
+Pkg.add("GraphRecipes")
+
+Pkg.add("RecipesBase")
+
+Pkg.add("IndexedTables")
+
+Pkg.add("Tables")
+
+Pkg.add("MultivariateStats")
+
+Pkg.add("RDatasets")
+
+Pkg.add("DataFrames")
+
+Pkg.add("CSV")
+
+ Pkg.add("DataValues")
+
+Pkg.installed()
+
+Pkg.update()
